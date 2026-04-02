813130fe4aed9bc840e0f03ab21f50863a7fa61c
jmsellitti1 <jmsellitti1@gmail.com>
2026-04-02 15:16:37 -0400

    Added more input features to data

--- scripts/load_data.py
+++ scripts/load_data.py
@@ -7,9 +7,13 @@ all_pitches = statcast('2025-03-27', '2025-09-28')
 all_pitches[:100].to_csv("data/2025_all_pitches.csv", index=False)
 
 in_play = all_pitches[(all_pitches['description'] == 'hit_into_play') & (~all_pitches['estimated_ba_using_speedangle'].isna())]
-data = in_play[['game_pk', 'home_team', 'batter', 'pitcher', 'launch_speed', 'launch_angle', 'estimated_ba_using_speedangle', 'events']]
-data = data.rename(columns = {'estimated_ba_using_speedangle': 'xBA', 'home_team': 'stadium'})
+data = in_play[[
+    'game_pk', 'home_team', 'away_team', 'batter', 'pitcher', 'launch_speed', 'launch_angle', 'hit_distance_sc', 'effective_speed',
+    'release_spin_rate', 'estimated_ba_using_speedangle', 'events'
+    ]]
+data = data.rename(columns = {'estimated_ba_using_speedangle': 'xBA',
+                              'hit_distance_sc': 'hit_distance',
+                              'effective_speed': 'pitch_speed'})
 data['hit'] = [1 if row['events'] in ['single', 'double', 'triple', 'home_run'] else 0 for index, row in data.iterrows()]
-data = data.reset_index(drop=True)
 
 data.to_csv("data/2025_hits.csv", index=False)
