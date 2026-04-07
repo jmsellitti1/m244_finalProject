32a1fdec79a1eaf251052fa21d20bcc254010d8b
jmsellitti1 <jmsellitti1@gmail.com>
2026-04-07 14:08:18 -0400

    Include classification script (better performing than regression)

---
+++ scripts/xba_classification.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix, classification_report
+
+data = pd.read_parquet("data/2025_hits.parquet")
+X = data[['launch_speed', 'launch_angle', 'hit_distance', 'hit_location_x', 'hit_location_y', 'pitch_speed', 'release_spin_rate']]
+y = data['hit']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+model = LogisticRegression(max_iter=1000)
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+
+conf_matrix = confusion_matrix(y_test, y_pred)
+print("Confusion Matrix:")
+print("[[TN, FP]")
+print(" [FN, TP]]")
+print(conf_matrix)
+class_report = classification_report(y_test, y_pred)
+print("Classification Report:")
+print(class_report)
