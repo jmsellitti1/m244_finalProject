660b4d538297af76e89856e6ae4eed3a6c7ea399
jmsellitti1 <jmsellitti1@gmail.com>
2026-04-07 14:44:19 -0400

    added data visualizations

--- scripts/xBA_regression.py
+++ scripts/xBA_regression.py
@@ -11,6 +11,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 model = LinearRegression()
 model.fit(X_train, y_train)
 y_pred = model.predict(X_test)
+y_pred = [max(0, min(pred, 1)) for pred in y_pred]
 mse = mean_squared_error(y_test, y_pred)
 r2 = r2_score(y_test, y_pred)
 print(f"Mean Squared Error: {mse}")
